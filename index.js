c029a6629d9b033e7ace2f3a97584cc462b6a7a3
neil-hughes <neil.hughes@techreturners.com>
2023-07-04 13:26:08 +0100

    feat: Implement Hello World

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("Hello, world.");
+
+// commit
+// add your files to the staging area?
+// adding your changes to files to a staging area?
+// allows you to push files
+
+// commit = group of changes
+// ahi
